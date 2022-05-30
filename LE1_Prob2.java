2803fb862ec85f1287f69b2ce38bdfe4682b10dd
ATUL SINGH <atulsinghoffical16@gmail.com>
2022-05-30 23:10:17 +0530

    java

---
+++ LE1_Prob2.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int n = 5;
+	    for(int i = 0; i < n; i++){
+	        for(int j = 0; j < i; j++)
+	            System.out.print(" ");
+	        for(int j = i; j < n; j++)
+	            System.out.print(" *");
+	        System.out.print("\n");
+	    }
+	}
+}
